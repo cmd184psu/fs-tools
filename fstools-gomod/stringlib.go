96bffa659e5c8f619cf6e5da095992c5906516ba
Chris Delezenski <cdelezenski@cloudian.coml>
2022-02-21 17:29:15 -0500

    added CSVtoArray to stringlib

--- fstools-gomod/stringlib.go
+++ fstools-gomod/stringlib.go
@@ -10,3 +10,15 @@ func TrimSuffix(s, suffix string) string {
 	}
 	return s
 }
+func CSVtoArray(tagcsv string) []string {
+	var tagcsv_array []string
+	if strings.Contains(tagcsv, ",") {
+		return strings.Split(tagcsv, ",")
+	} else if len(tagcsv) > 0 {
+		tagcsv_array = make([]string, 1)
+		tagcsv_array[0] = tagcsv
+	} else {
+		tagcsv_array = make([]string, 0)
+	}
+	return tagcsv_array
+}
